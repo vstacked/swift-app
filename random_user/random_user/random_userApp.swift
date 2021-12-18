31c1cb5024166e5f0b81a875aacd216a5a07d44b
rizki <54944887+vstacked@users.noreply.github.com>
2021-12-18 14:32:30 +0700

    feat: :tada: initial

---
+++ random_user/random_user/random_userApp.swift
@@ -0,0 +1,17 @@
+//
+//  random_userApp.swift
+//  random_user
+//
+//  Created by vstacked on 18/12/21.
+//
+
+import SwiftUI
+
+@main
+struct random_userApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
